12fc29ef72bdbbb19583262ebe1a76778670bc96
alexaoh <alex.andunge@gmail.com>
2019-12-28 20:15:31 +0100

    Started Oving2. In the middle of 2c now.

--- Oving1/main.cpp
+++ Oving1/main.cpp
@@ -1,21 +1,31 @@
-//
-// This is example code from Chapter 2.2 "The classic first program" of
-// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
-// 
-// keep_window_open() added for TDT4102, excercise 0
+#include "Simple_window.h"
 
-// This program outputs the message "Hello, World!" to the monitor
-
-#include "std_lib_facilities.h"
-
-//------------------------------------------------------------------------------'
-
-// C++ programs start by executing the function main
-int main()
-{
-	cout << "Hello, World!\n";
-
-	keep_window_open();
+int maxOfTwo(int a, int b){
+	if (a>b){
+		cout << "A is greater than B" << '\n';
+		return a;
+	} else {
+		cout << "B is greater than or equal to A" << '\n';
+		return b;
+	} 
 }
 
-//------------------------------------------------------------------------------
+int fibonacci(int n)[
+	int a = 0;
+	int b = 1;
+	cout << "Fibonacci numbers:" << '\n';
+	for (int x = 1; x < n + 1; x += 1){
+		cout << x << b;
+		int temp = b;
+		b += a;
+		a = temp;
+	}
+	cout << "----";
+	return b;
+}
+
+int main(){
+	cout << "Oppgave a)\n";
+	cout << maxOfTwo(5,6) << '\n';
+	Kommenter ut dette! Finn ut hvordan man kan kommentere!
+}
