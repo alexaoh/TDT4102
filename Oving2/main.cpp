8e50e6deb023f61069eafb27b08283b898a19ca2
alexaoh <alex.andunge@gmail.com>
2020-01-06 22:03:46 +0100

    Finished task1 on Oving2

--- Oving2/main.cpp
+++ Oving2/main.cpp
@@ -1,8 +1,59 @@
 #include "std_lib_facilities.h"
 
+//Task 1: 
+void inputAndPrintInteger(){
+	cout << "Skriv inn et tall: ";
+	int x = 0;
+	cin >> x;
+	cout << "Du skrev: " << x << endl;
+}
+
+int inputInteger(){
+	cout << "Skriv inn et tall: ";
+	int x = 0;
+	cin >> x;
+	return x;
+}
+
+void inputIntegersAndPrintSum(){
+	/*Teori d): Jeg valgte å bruke inputInteger fordi
+				den andre printer på en annen måte enn 
+				det som er ønskelig i denne oppgaven!
+				Jeg trenger kun input fra bruker. 
+	*/
+	int x = inputInteger();
+	int y = inputInteger();
+	int sum = x + y;
+	cout << "Summen av tallene: " << sum <<  endl;
+}
+
+bool isOdd(int n){
+	if (n%2 == 1){
+		return true;
+	}
+	return false;
+}
+
+void testIsOdd(){
+	//I made this function to test isOdd in the interval [0,15]
+	for (int i = 0; i <= 15; i++){
+		bool x = isOdd(i);
+		cout << "Tall: " << i << " gir " << std::boolalpha << x << endl;
+	}
+}
+
+void printHumanReadableTime(int sec){
+	int hours = sec / 3600;
+	int min = (sec%3600) / 60;
+	sec = sec % 60;
+	cout << hours << " timer " << min << " minutter " << sec << " sekunder " << endl;
+}
+
+//Task 1 ends here. 
+
+//Task 2, Løkker:
 int main()
 {
-	cout << "Hello, World!\n";
-
+	
 	keep_window_open();
 }
