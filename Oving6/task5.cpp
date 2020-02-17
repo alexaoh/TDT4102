4d5f7bacbf547eed2c4e3c01429c138f170e0c8b
alexaoh <alex.andunge@gmail.com>
2020-02-17 19:33:09 +0100

    Finished Oving6!

--- Oving6/task5.cpp
+++ Oving6/task5.cpp
@@ -46,23 +46,17 @@ const int findMaxTemp(vector<Temps>& readTemps){
 }
 
 
-Scale xs{xoffset, base_temp, xscale};
-Scale ys{ymax-yoffset, base_temp, -yscale};
+Scale xs{xoffset, 0, xscale};
+Scale ys{(ymax-yoffset)/2, 0, -yscale};
 
 void buildGraph(){
     Window win{Point{100, 100}, xmax, ymax, "Daglige maks- og minimumstemperaturer fra 3.februar 2018 til og med 3.februar 2019"};
 
-    const int months{12};
     Axis x{Axis::x, Point{xoffset, (ymax-yoffset)/2}, xlength, months, 
-    "            Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec   Jan   "};
-    x.label.move(-100, 0);
+    " Feb        Mar        Apr       May       Jun        Jul        Aug        Sep        Oct         Nov        Dec        Jan   "};
+    x.label.move(-150, 0);
 
-    Axis y{Axis::y, Point{xoffset, ymax-yoffset}, ylength, 5, "degrees (Centigrade)"};
-
-    /* Unødvendig
-    Line current_temp{Point{xs(6), ys(0)}, Point{xs(6), ys(100)}}; //Wrong numbers here. 
-    current_temp.set_style(Line_style::dash);
-    */
+    Axis y{Axis::y, Point{xoffset, ymax-yoffset}, ylength, 0, "degrees (Centigrade)"};
 
     Open_polyline max_temps; 
     Open_polyline min_temps; 
@@ -83,13 +77,24 @@ void buildGraph(){
     max_temps.set_color(Color::red);
     min_temps.set_color(Color::blue);
 
-    Text Max{Point{xoffset/2, ymax/2}, "Max"};
+    Text Max{Point{xoffset/2, max_temps.point(10).y}, "Max"};
     Max.set_color(Color::red);
 
-    Text Min{Point{xoffset/2, ymax/2+20}, "Min"};
+    Text Min{Point{xoffset/2, min_temps.point(10).y}, "Min"};
     Min.set_color(Color::blue);
 
-    //Need 5 more texts for values on y-axis!
+    const double movePoints{-20.0};
+    Text minusTwenty{Point{xs(0), ys(-20)}, "-20"};
+    minusTwenty.move(movePoints, 0);
+    Text minusTen{Point{xs(0), ys(-10)}, "-10"};
+    minusTen.move(movePoints, 0);
+    Text zero{Point{xs(0), ys(0)}, "0"};
+    zero.move(movePoints,0);
+    Text Ten{Point{xs(0), ys(10)}, "10"};
+    Ten.move(movePoints, 0);
+    Text Twenty{Point{xs(0), ys(20)}, "20"};
+    Twenty.move(movePoints, 0);
+
 
     //attach axes
     win.attach(x);
@@ -102,6 +107,13 @@ void buildGraph(){
     //attach texts
     win.attach(Max);
     win.attach(Min);
+
+    //attach points
+    win.attach(minusTwenty);
+    win.attach(minusTen);
+    win.attach(zero);
+    win.attach(Ten);
+    win.attach(Twenty);
 
 
     gui_main();
--- Oving6/task5.h
+++ Oving6/task5.h
@@ -1,8 +1,8 @@
 #include "task4.h"
 #include "std_lib_facilities.h"
 
-constexpr int xmax = 505;
-constexpr int ymax = 400;
+constexpr int xmax = 950;
+constexpr int ymax = 650;
 
 constexpr int xoffset = 100;
 constexpr int yoffset = 60;
@@ -11,9 +11,10 @@ constexpr int xspace = 40;
 constexpr int yspace = 40;
 
 //Length of axes 
-constexpr int xlength = xmax-xoffset-xspace;
+constexpr int xlength = xmax-xoffset-xspace; //365 days in the year
 constexpr int ylength = ymax-yoffset-yspace;
 
+const int months{12};
 
 vector<Temps> readAllTempsFromFile(string filename);
 const int findMinTemp(vector<Temps>& readTemps);
@@ -28,14 +29,13 @@ const int end_temp = findMaxTemp(readings);
 cout << "Max temp: " << to_string(end_temp) << "| Min temp: " << to_string(base_temp) << endl;
 */
 
-//guess some values
-const int base_temp = -14;
-const int end_temp = 35;
+//guess some values --> Ville egt finne max og min
+const int base_temp = -40;
+const int end_temp = 40;
 
-constexpr double xscale = double(xlength)/(end_temp-base_temp);
-constexpr double yscale = double(ylength)/100;
+constexpr double xscale = double(xlength)/365;
+constexpr double yscale = double(ylength)/(end_temp-base_temp);
 
-//need to fix some scaling!
 class Scale{
     int cbase; 
     int vbase;
